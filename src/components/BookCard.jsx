928ea7e1945a7c9f206dc1fdc6219edf5e25d028
tanjidahmad <tanjidahmad.cse.uiu@gmail.com>
2026-05-02 12:25:27 -0400

    details

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -37,7 +37,7 @@ const BookCard = ({book}) => {
             </div>
            </div> */}
 
-     <Link href={`/all-books/${book.id}`}>      <Button variant="outline" className={'w-full'}>View</Button></Link>
+     <Link href={`/all-books/${book.id}`}>      <Button variant="outline" className={'w-full'}>Details</Button></Link>
             
         </Card>
     );
